5b0271c580a20eee45fd6c4cf020f3a4394d176f
Gerald Ezenagu <egerald344@gmail.com>
2023-01-20 20:36:25 +0100

    added state and context

--- component/comments/CommentsList.js
+++ component/comments/CommentsList.js
@@ -1,15 +1,20 @@
 import BigEnd from "../UI/BigContent/BigEnd";
 
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
+import { PostContext } from "../../hook/context-hook";
 import classes from "./Comments.module.css";
 import Replies from "../UI/smallContent/Replies/Replies";
 import CommentForm from "./CommentForm";
 import BigReply from "../UI/BigContent/BigReply/BigReply";
 import Contents from "../UI/smallContent/Contents";
 
-const CommentList = ({ details, creator, reply, show, onClose }) => {
+
+const CommentList = ({ creator, show, onClose }) => {
+
+  const {comment, replys} = useContext(PostContext);
+  
   const [hideButton, setHideButton] = useState(null);
   const [hideBigButton, setHideBigButton] = useState(null);
 
@@ -24,7 +29,7 @@ const CommentList = ({ details, creator, reply, show, onClose }) => {
   return (
     <>
       {/* by putting a question mark ? in front of .map we ensure that an error isn't thrown if the value is undefined */}
-      {details?.map((value, id) => {
+      {comment?.map((value, id) => {
         return (
           <React.Fragment key={uuidv4()}>
             <div key={uuidv4()} className={classes.smallContent}>
@@ -53,14 +58,15 @@ const CommentList = ({ details, creator, reply, show, onClose }) => {
                   onClose={onClose}
                   replyTo={value.user.username}
                   creator={creator}
-                  replies={reply}
+                  replies={replys}
                 />
               </div>
             </div>
           </React.Fragment>
         );
       })}
-      {details?.map((val, id) => {
+
+      {comment?.map((val, id) => {
         return (
           <React.Fragment key={uuidv4()}>
             <div key={uuidv4()} className={classes.bigContent}>
@@ -88,7 +94,7 @@ const CommentList = ({ details, creator, reply, show, onClose }) => {
                   show={show}
                   onClose={onClose}
                   replyTo={val.user.username}
-                  replies={reply}
+                  replies={replys}
                   creator={creator}
                 />
               </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { createContext, useContext } from "react";
+import React, { useState, useEffect } from "react";
+import { PostContext } from "../hook/context-hook";
 // our-main-domain/
 import CommentList from "../component/comments/CommentsList";
 import "../styles/home.module.css";
 import "@fontsource/rubik";
-import React, { useState, useEffect } from "react";
+
 import LoadingSpinner from '../component/UI/LoadingSpinner/LoadingSpinner'
 
 import { initMongoose } from "../lib/mongoose";
@@ -15,6 +18,8 @@ import Modal from "../component/UI/Modal/Modal";
 
 
 function Homepage({ comments, currentUser, replies }) {
+
+  
   const [verify, setVerify] = useState(true);
   
 
@@ -23,18 +28,71 @@ function Homepage({ comments, currentUser, replies }) {
   const hideModal = () => setShowModal(false);
   const openModal = () => setShowModal(true);
 
+
+  const [comment, setComment] = useState(comments);
+  const [replys, setReplys] = useState(replies);
+  
+
+  
+  
+  // // get new comment created
+  // const getNewState = (value) => {
+  //   const newValue = { ...value };
+  //   setComments([...response, newValue]);
+  // };
+
+  // // get comment edited
+  // const getEditContent = (value) => {
+  //   setComments((com) => {
+  //     const filtered = com?.filter((del) => del._id !== value._id);
+  //     return [...filtered, value];
+  //   });
+  // };
+
+  // // getDeletedComment
+  // const deleteContents = (id) => {
+  //   setComments((com) => com?.filter((del) => del._id !== id));
+  //   console.log(comments);
+  // };
+
+  
+  // // get new reply created
+  // const getNewReply = (value) => {
+  //   const newValue = { ...value };
+  //   setResponse([...response, newValue]);
+  // };
+
+  // // get reply edited
+  // const getEditReply = (value) => {
+  //   setResponse((response) => {
+  //     const filtered = response?.filter((del) => del._id !== value._id);
+  //     return [...filtered, value];
+  //   });
+  // };
+
+  // // getDeletedReply
+  // const deleteReply = (id) => {
+  //   setResponse((response) => response?.filter((del) => del._id !== id));
+  //   console.log(response);
+  // };
+
+
   return (
-    
+    <PostContext.Provider value={{comment, setComment, replys, setReplys}}>
       <div>
         <Modal show={showModal} > <LoadingSpinner /></Modal>
-        <CommentList show={openModal} onClose={hideModal} details={comments} creator={currentUser} reply={replies} />
+        {!!comments && <CommentList show={openModal} onClose={hideModal} creator={currentUser} /> }
         <PostComment show={openModal} onClose={hideModal} creator={currentUser}  />
       </div>
-        
+        </PostContext.Provider>
   );
 }
 
 export default Homepage;
+
+
+
+
 
 export async function getServerSideProps() {
   await initMongoose();
