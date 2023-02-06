59765073d702e5111ae1acd19f2829e8f62f02b6
Gerald Ezenagu <egerald344@gmail.com>
2023-02-06 23:38:11 +0100

    deploy01

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import "../styles/globals.css";
-import * as dotenv from 'dotenv'
-dotenv.config()
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
